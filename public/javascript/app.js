9ebc2efa1d416fd85cfc61b3341cbaea26cc39c3
Lubo7 <lkramoris@gmail.com>
2018-08-01 19:25:47 +0200

    myfirstcommit

---
+++ public/javascript/app.js
@@ -0,0 +1,6 @@
+var myFullpage = new fullpage('#fullpage', {
+    anchors: ['section_0', 'section_1', 'section_2', 'section_3', 'section_4'],
+    navigation: true,
+    navigationPosition: 'right',
+    navigationTooltips: ['Home','Services','References','About me','Contact']
+});
